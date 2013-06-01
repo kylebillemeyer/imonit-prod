63d54abd20d0fe8dba6248657a3bbf962c17532e
Kyle Billemeyer <kyle@localhost.localdomain>
2013-06-01 13:22:52 -0400

    Initial repository set up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImonitProd::Application.config.secret_token = 'f5a51e5b22ecbbdd312f6c30261abee408f9f7ebe2ce008352b71d5193a7e2da96c7d3cfcac376e888bfb7c464bb4b04cb9d0bccf30bd78a583cb7f2f49f5da7'
